77a5cd1782f9d3f72ab9bec7d9ebf262a4601c01
mateuszpsnik <matikolbudy@gmail.com>
2020-10-13 18:23:30 +0200

    Basic project structure created

---
+++ SieciProjekt1/Model/Packet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SieciProjekt1.Model
+{
+    public class Packet
+    {
+
+    }
+}
---
+++ SieciProjekt1/ViewModel/UploadedFileViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SieciProjekt1.Model;
+
+namespace SieciProjekt1.ViewModel
+{
+    public class UploadedFileViewModel
+    {
+
+    }
+}
